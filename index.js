df60f50b5359b1a27a400dfb9a40b1893ad5ffae
Fabio Ferreira <fabio35547@gmail.com>
2021-06-24 20:12:07 -0300

    Iniciando o exercicio

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const bodyParser = require('body-parser');
-
 const express = require('express');
+const bodyParser = require('body-parser');
+require('dotenv').config();
 
 const app = express();
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Port: ${PORT}`);
